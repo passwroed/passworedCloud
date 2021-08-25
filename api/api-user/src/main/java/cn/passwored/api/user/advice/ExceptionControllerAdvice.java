95b5d532bd2c40fe8912ecccaea728f0147e59e8
wangke <wangke@fuma8.com>
2021-08-26 02:00:10 +0800

    添加用户服务

--- api/api-user/src/main/java/cn/passwored/api/user/advice/ExceptionControllerAdvice.java
+++ api/api-user/src/main/java/cn/passwored/api/user/advice/ExceptionControllerAdvice.java
@@ -1,5 +1,9 @@
 package cn.passwored.api.user.advice;
 
+import cn.passwored.commons.core.bean.ApiResponse;
+import cn.passwored.commons.core.bean.ApiStatus;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,11 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Author wangke
  */
 @ControllerAdvice
+@Slf4j
 public class ExceptionControllerAdvice {
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public String error(Exception e) {
-        e.printStackTrace();
-        return "{}";
+        log.error(e.getStackTrace().toString());
+        return JSON.toJSONString(ApiResponse.buildMessageCode(e.getMessage(), ApiStatus.SC_INTERNAL_SERVER_ERROR));
     }
 }
